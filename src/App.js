d5ef7e1f5ada77d27679ebcf57bef653c31d5009
shidingchen <shidingS1>
2020-02-11 21:50:42 -0500

    final

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
 import "./App.css";
+import { Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Routes from "./Routes";
 
 export default class App extends Component {
-  render() {
-    return<div>Fisher Bookstore</div>;
+  render(){
+    return (
+      <div className="App container">
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand>
+            <Link to="/">Fisher Bookstore</Link>
+          </Navbar.Brand>
+        </Navbar>
+        <Routes />
+      </div>
+    );
   }
 }
