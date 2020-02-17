5ca7fd07e9ed7254c0bf64a55aea413f5a8fd7bd
shidingchen <59670803+shidingchen@users.noreply.github.com>
2020-02-17 16:53:29 -0500

    1 (#19)

---
+++ src/books/Books.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Books.css";
+
+export default function Books() {
+    return(
+        <div className="Books">
+            <div className="lander">
+                <h1>Books</h1>
+                <p>Name of the Book.</p>
+            </div>
+        </div>
+    );
+}
