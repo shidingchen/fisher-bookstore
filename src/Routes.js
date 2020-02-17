8be08fc00f1cbb3ce02eac99e80bf179ecb3b9d8
shidingchen <59670803+shidingchen@users.noreply.github.com>
2020-02-17 18:12:43 -0500

    1 (#20)

--- src/Routes.js
+++ src/Routes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./home/Home";
 import Books from "./books/Books";
+import Authors from "./authors/Authors";
 
 export default function Routes() {
     return(
         <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/Books" component={Books} />
+            <Route path="/Authors" component={Authors} />
         </Switch>
     );
 }
