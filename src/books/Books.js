0437fe8f9e07c702b1bbe491bfce67bf2a139e86
shidingchen <shidingS1>
2020-02-19 18:42:26 -0500

    1

--- src/books/Books.js
+++ src/books/Books.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { Component} from "react";
 import "./Books.css";
+import { BookDisplay } from "./BookDisplay";
 
-export default function Books() {
+
+export default class Books extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            books: [
+                {
+                    id: 1,
+                    title: "Domain Driven Design",
+                    author: "Eric Evans",
+                    isbn: "978-0321125217"
+                },
+                {
+                    id:2,
+                    title: "Accelerate",
+                    author:"Nicole Forsgren",
+                    isbn: "978-1942788331"
+                }
+            ]
+        }
+    };
+
+render(){
     return(
         <div className="Books">
             <div className="lander">
-                <h1>Books</h1>
-                <p>Name of the Book.</p>
+                <BookDisplay books={this.state.books} />
             </div>
         </div>
     );
+}
 }
