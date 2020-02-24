eb5ae9d5bf14c45f7323206e21463edd461dcc5d
shidingchen <shidingS1>
2020-02-24 18:12:57 -0500

    12

--- src/books/Books.js
+++ src/books/Books.js
@@ -19,6 +19,42 @@ export default class Books extends Component {
                     title: "Accelerate",
                     author:"Nicole Forsgren",
                     isbn: "978-1942788331"
+                },
+                {
+                    id:3,
+                    title: "Kobe:Forever",
+                    author:"Los Angeles Daily News",
+                    isbn: "978-1629378503"
+                },
+                {
+                    id:4,
+                    title: "Lost",
+                    author:"James Patterson",
+                    isbn: "978-0316420327"
+                },
+                {
+                    id:5,
+                    title: "Before I Met You:A Novel",
+                    author:"Lisa Jewell",
+                    isbn: "978-1476702940"
+                },
+                {
+                    id:6,
+                    title: "Fatal:A Novel",
+                    author:"John Lescroart",
+                    isbn: "978-1501115684"
+                },
+                {
+                    id:7,
+                    title: "Dead Letters",
+                    author:"Caite Dolan-Leach",
+                    isbn: "978-0399588877"
+                },
+                {
+                    id:8,
+                    title: "Magnolia City",
+                    author:"Duncan W.Alderson",
+                    isbn: "978-0758292759"
                 }
             ]
         }
